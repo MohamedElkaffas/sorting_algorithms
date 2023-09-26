2e8b4a00f2aac091bf5dbc2be5cd72c89f924eb9
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-09-26 02:15:19 +0000

    selection

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+  * selection_sort - O(n^2)
+  * @array: array
+  * @size: sz
+  * Return: void
+  */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 1, aux = 0, min = 0, limit = size - 1;
+
+	if (size < 2)
+		return;
+
+	while (i < limit)
+	{
+		if (j == size)
+		{
+			if (i != min)
+			{
+				aux = array[i];
+				array[i] = array[min], array[min] = aux;
+				print_array(array, size);
+			}
+
+			++i, min = i;
+			j = i + 1;
+			continue;
+		}
+
+		if (array[min] > array[j])
+			min = j;
+
+		++j;
+	}
+}
