05fb6ac18fe1eb053b827e7932fc84f80890f1a2
MohamedElkaffas <mohamedalkaffas2000@gmail.com>
2023-09-26 03:16:02 +0000

    hoare_quick_comments

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+/**
+  * quick_sort - fast n log n average and best cases, 
+  * hoare partition
+  * @array: array
+  * @size: sz
+  * Return: void
+  */
+
 void quick_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
@@ -7,6 +15,15 @@ void quick_sort(int *array, size_t size)
 
 	quick_sort_rec(array, 0, size - 1, size);
 }
+
+/**
+  * quick_sort_rec - define recursively
+  * @array: array
+  * @lower: low
+  * @higher: high
+  * @size: sz
+  * Return: void
+  */
 
 void quick_sort_rec(int *array, int lower, int higher, size_t size)
 {
@@ -19,6 +36,16 @@ void quick_sort_rec(int *array, int lower, int higher, size_t size)
 		quick_sort_rec(array, l_p + 1, higher, size);
 	}
 }
+
+
+/**
+  * hoare_partition - Swapping with pivot being regarded as a reference pt
+  * @array: arr
+  * @lower: low
+  * @higher: high
+  * @size: sz
+  * Return: void
+  */
 
 int hoare_partition(int *array, int lower, int higher, size_t size)
 {
